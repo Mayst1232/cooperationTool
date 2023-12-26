2ec6669b0ad0a9c4a6190b4e1abe57bd31244703
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-26 15:50:49 +0900

    first commit

---
+++ src/main/java/com/example/cooperationtool/CooperationToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.cooperationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperationToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooperationToolApplication.class, args);
+    }
+
+}
