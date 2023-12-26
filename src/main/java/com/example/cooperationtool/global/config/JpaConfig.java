87f81fb22de809826997e1e7e76d39fd2b71a643
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-26 16:27:35 +0900

    feat: add jpa configuration

---
+++ src/main/java/com/example/cooperationtool/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.example.cooperationtool.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
