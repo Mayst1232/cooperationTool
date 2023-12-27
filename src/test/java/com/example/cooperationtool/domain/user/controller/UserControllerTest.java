c48355262585eb364590b8cf40d76da48fa73528
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-27 19:37:26 +0900

    refactory: Controller 테스트 통합테스트에서 단위테스트로 변경

--- src/test/java/com/example/cooperationtool/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/example/cooperationtool/domain/user/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.cooperationtool.domain.user.controller;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -10,20 +11,33 @@ import com.example.cooperationtool.domain.user.dto.request.SignupRequestDto;
 import com.example.cooperationtool.domain.user.dto.response.SignupResponseDto;
 import com.example.cooperationtool.domain.user.entity.UserRoleEnum;
 import com.example.cooperationtool.domain.user.service.UserService;
+import com.example.cooperationtool.domain.user.support.MockSpringSecurityFilter;
+import com.example.cooperationtool.global.config.WebSecurityConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(
+    controllers = {UserController.class},
+    excludeFilters = {
+        @ComponentScan.Filter(
+            type = FilterType.ASSIGNABLE_TYPE,
+            classes = WebSecurityConfig.class
+        )
+    }
+)
 @ActiveProfiles("test")
 class UserControllerTest {
 
@@ -32,8 +46,19 @@ class UserControllerTest {
     @Autowired
     protected MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext context;
+
     @MockBean
     UserService userService;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(springSecurity(new MockSpringSecurityFilter()))
+            .alwaysDo(print())
+            .build();
+    }
 
     @Nested
     @DisplayName("유저 회원가입")
