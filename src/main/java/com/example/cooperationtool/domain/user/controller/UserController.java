f4115d00ad8ae6176bc9057d4cdfe35f1292eef1
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-27 23:37:33 +0900

    feat: add get user profile API Controller

--- src/main/java/com/example/cooperationtool/domain/user/controller/UserController.java
+++ src/main/java/com/example/cooperationtool/domain/user/controller/UserController.java
@@ -3,16 +3,20 @@ package com.example.cooperationtool.domain.user.controller;
 import static com.example.cooperationtool.global.exception.ErrorCode.SIGNUP_FAIL;
 
 import com.example.cooperationtool.domain.user.dto.request.SignupRequestDto;
+import com.example.cooperationtool.domain.user.dto.response.ProfileResponseDto;
 import com.example.cooperationtool.domain.user.dto.response.SignupResponseDto;
 import com.example.cooperationtool.domain.user.service.UserService;
 import com.example.cooperationtool.global.dto.response.RootResponseDto;
 import com.example.cooperationtool.global.exception.ServiceException;
+import com.example.cooperationtool.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,4 +45,16 @@ public class UserController {
             .data(responseDto)
             .build());
     }
+
+    @GetMapping("/user/profile")
+    public ResponseEntity<?> getProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        ProfileResponseDto responseDto = userService.getProfile(userDetails.getUser());
+
+        return ResponseEntity.ok(RootResponseDto.builder()
+            .code("200")
+            .message("프로필 조회 성공")
+            .data(responseDto)
+            .build());
+    }
+
 }
