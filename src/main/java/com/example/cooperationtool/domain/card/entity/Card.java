3d55f5eabea9180daa3066311b3408f904142c8f
JaeHoon <wogns8030@naver.com>
2023-12-27 22:46:33 +0900

    save changes

---
+++ src/main/java/com/example/cooperationtool/domain/card/entity/Card.java
@@ -0,0 +1,34 @@
+package com.example.cooperationtool.domain.card.entity;
+
+import com.example.cooperationtool.domain.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Card extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    public void setCard(String title){
+        this.title = title;
+    }
+
+}
