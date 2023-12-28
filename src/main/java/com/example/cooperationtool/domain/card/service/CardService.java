9330778919da1ec2e132ab32aa2d707f0664dec4
JaeHoon <wogns8030@naver.com>
2023-12-28 11:18:03 +0900

    feat : User 확인 Service

--- src/main/java/com/example/cooperationtool/domain/card/service/CardService.java
+++ src/main/java/com/example/cooperationtool/domain/card/service/CardService.java
@@ -4,7 +4,9 @@ import com.example.cooperationtool.domain.card.dto.CardRequestDto;
 import com.example.cooperationtool.domain.card.dto.CardResponseDto;
 import com.example.cooperationtool.domain.card.entity.Card;
 import com.example.cooperationtool.domain.card.exception.NotFoundCardException;
+import com.example.cooperationtool.domain.card.exception.NotFoundWorker;
 import com.example.cooperationtool.domain.card.repository.CardRepository;
+import com.example.cooperationtool.domain.user.entity.User;
 import com.example.cooperationtool.global.dto.response.RootResponseDto;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,12 +21,18 @@ public class CardService {
 
     private final CardRepository cardRepository;
 
-    public RootResponseDto createCard(CardRequestDto cardRequestDto) {
-        Card card = cardRepository.save(Card.builder().title(cardRequestDto.getTitle()).build());
+    public RootResponseDto createCard(CardRequestDto cardRequestDto, User user) {
+        findByUser(user);
+        Card card = cardRepository.save(Card.builder()
+            .user(user)
+            .title(cardRequestDto.getTitle())
+            .build());
+
         return new RootResponseDto("200","생성 완료",card);
     }
 
-    public List<CardResponseDto> getCards() {
+    public List<CardResponseDto> getCards(User user) {
+        findByUser(user);
         List<Card> cardList = cardRepository.findAll();
         return cardList.stream()
             .map(CardResponseDto::of)
@@ -32,14 +40,16 @@ public class CardService {
     }
 
     @Transactional(readOnly = true)
-    public RootResponseDto getCard(Long cardId) {
-        Card card = extracted(cardId);
+    public RootResponseDto getCard(Long cardId, User user) {
+        findByUser(user);
+        Card card = findByCard(cardId);
         return new RootResponseDto("200","조회 완료", card);
     }
 
     @Transactional
-    public RootResponseDto modifyCard(Long cardId, CardRequestDto requestDto) {
-        Card card = extracted(cardId);
+    public RootResponseDto modifyCard(Long cardId, CardRequestDto requestDto, User user) {
+        findByUser(user);
+        Card card = findByCard(cardId);
         if(card != null){
             Card modifyCard = cardRepository.save(Card.builder().title(requestDto.getTitle()).build());
             return new RootResponseDto("200","수정 성공",modifyCard);
@@ -49,8 +59,9 @@ public class CardService {
     }
 
     @Transactional
-    public void deleteCard(Long cardId) {
-        Card card = extracted(cardId);
+    public void deleteCard(Long cardId, User user) {
+        findByUser(user);
+        Card card = findByCard(cardId);
         if(card != null){
             cardRepository.deleteById(cardId);
             ResponseEntity.ok().body("성공적으로 삭제되었습니다.");
@@ -59,7 +70,13 @@ public class CardService {
         }
     }
 
-    private Card extracted(Long cardId) {
+    private void findByUser(User user) {
+        cardRepository.findById(user.getId()).orElseThrow(
+            () -> new NotFoundWorker("userId", user.getId().toString(),"해당 ID가 없습니다.")
+        );
+    }
+
+    private Card findByCard(Long cardId) {
         return cardRepository.findById(cardId).orElseThrow(
             () -> new NotFoundCardException("cardId", cardId.toString(),"Card를 찾을 수 없습니다.")
         );
