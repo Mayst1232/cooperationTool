87681a85d66cc2422160ea2a59a456dad1e0617d
JaeHoon <wogns8030@naver.com>
2023-12-28 16:07:28 +0900

    fix : Entity Builder add

--- src/main/java/com/example/cooperationtool/domain/card/dto/CardRequestDto.java
+++ src/main/java/com/example/cooperationtool/domain/card/dto/CardRequestDto.java
@@ -1,13 +1,16 @@
 package com.example.cooperationtool.domain.card.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CardRequestDto {
 
     @NotBlank
