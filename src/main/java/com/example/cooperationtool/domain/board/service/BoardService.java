5bf98025c09702c38e4d5262b46feb94c593e526
minhyeongKang <kang7937@gmail.com>
2023-12-28 11:18:15 +0900

    feat : add board-delete Service complete

--- src/main/java/com/example/cooperationtool/domain/board/service/BoardService.java
+++ src/main/java/com/example/cooperationtool/domain/board/service/BoardService.java
@@ -1,10 +1,15 @@
 package com.example.cooperationtool.domain.board.service;
 
+import static com.example.cooperationtool.global.exception.ErrorCode.NOT_FOUND_BOARD;
+import static com.example.cooperationtool.global.exception.ErrorCode.NOT_MATCH_USER;
+
 import com.example.cooperationtool.domain.board.dto.request.BoardRequestDto;
 import com.example.cooperationtool.domain.board.dto.response.BoardResponseDto;
 import com.example.cooperationtool.domain.board.entity.Board;
 import com.example.cooperationtool.domain.board.repository.BoardRepository;
 import com.example.cooperationtool.domain.user.entity.User;
+import com.example.cooperationtool.global.exception.ServiceException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,4 +31,13 @@ public class BoardService {
         return new BoardResponseDto(board);
     }
 
+    public void deleteBoard(Long boardId, User user) {
+        Board board = boardRepository.findById(boardId)
+            .orElseThrow(() -> new ServiceException(NOT_FOUND_BOARD));
+        if (!user.equals(board.getUser())) {
+            throw new ServiceException(NOT_MATCH_USER);
+        }
+
+        boardRepository.delete(board);
+    }
 }
