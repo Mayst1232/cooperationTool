c040ebe667ad934eb087b2a117879979f675226b
minhyeongKang <kang7937@gmail.com>
2023-12-28 11:19:17 +0900

    feat : add ErrorCode for board-delete Exception

--- src/main/java/com/example/cooperationtool/global/exception/ErrorCode.java
+++ src/main/java/com/example/cooperationtool/global/exception/ErrorCode.java
@@ -13,10 +13,11 @@ public enum ErrorCode {
     DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "1001", "중복된 사용자 이름입니다."),
     WRONG_ADMIN_CODE(HttpStatus.BAD_REQUEST, "1002", "관리자 암호가 틀려 등록이 불가능합니다."),
     LOGIN_FAILED(HttpStatus.BAD_REQUEST, "1003", "로그인에 실패했습니다."),
-    TOKEN_ERROR(HttpStatus.BAD_REQUEST, "1004", "토큰이 틀립니다.")
+    TOKEN_ERROR(HttpStatus.BAD_REQUEST, "1004", "토큰이 틀립니다."),
 
     // board (2000)
-
+    NOT_FOUND_BOARD(HttpStatus.BAD_REQUEST, "2001", "요청하신 게시글이 존재하지 않습니다."),
+    NOT_MATCH_USER(HttpStatus.BAD_REQUEST, "2002", "작성자만 수정 및 삭제를 할 수 있습니다.")
     // column (3000)
 
     // card (4000)
