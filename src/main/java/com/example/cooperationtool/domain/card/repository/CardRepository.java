a661f0be2049e701d7cc1501eff85bf891fb6f82
JaeHoon <wogns8030@naver.com>
2023-12-28 20:10:24 +0900

    feat : SQlMethod

--- src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
+++ src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
@@ -1,10 +1,13 @@
 package com.example.cooperationtool.domain.card.repository;
 
 import com.example.cooperationtool.domain.card.entity.Card;
+import com.example.cooperationtool.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card,Long> {
 
+    Optional<Card> findByIdAndUser(Long cardId, User user);
 }
