7f9fcd878318c260a088976a4e024a5e79335287
JaeHoon <wogns8030@naver.com>
2023-12-28 20:10:24 +0900

    feat : SQlMethod

--- src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
+++ src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CardRepository extends JpaRepository<Card,Long> {
 
+    Optional<Card> findByIdAndUser(Long cardId, User user);
 }
