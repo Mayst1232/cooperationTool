0789fa1939bbdaa0f317ba384ab9c518004a76cd
JaeHoon <wogns8030@naver.com>
2023-12-28 10:13:17 +0900

    feat : Repository구현

--- src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
+++ src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
@@ -2,7 +2,9 @@ package com.example.cooperationtool.domain.card.repository;
 
 import com.example.cooperationtool.domain.card.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CardRepository extends JpaRepository<Card,Long> {
 
 }
