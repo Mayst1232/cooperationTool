5f46ae202ac817775aadc621069f054f5eaa2615
JaeHoon <wogns8030@naver.com>
2023-12-29 13:14:45 +0900

    refactoring : ResponseDto 수정

--- src/main/java/com/example/cooperationtool/domain/card/dto/CardResponseDto.java
+++ src/main/java/com/example/cooperationtool/domain/card/dto/CardResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.cooperationtool.domain.card.dto;
 
 import com.example.cooperationtool.domain.card.entity.Card;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,16 +13,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @NoArgsConstructor
-public class CardResponseDto{
+public class CardResponseDto {
 
-    private Long id;
+    private Long cardId;
     private String title;
+    @JsonInclude
     private LocalDateTime createdAt;
+    @JsonInclude(Include.NON_NULL)
     private LocalDateTime modifiedAt;
+
+    public CardResponseDto(Card card) {
+        this.cardId = card.getId();
+        this.title = card.getTitle();
+        this.createdAt = card.getCreatedAt();
+        this.modifiedAt = card.getModifiedAt();
+    }
 
     public static CardResponseDto of(Card card) {
         return CardResponseDto.builder()
-            .id(card.getId())
+            .cardId(card.getId())
             .title(card.getTitle())
             .createdAt(card.getCreatedAt())
             .modifiedAt(card.getModifiedAt())
