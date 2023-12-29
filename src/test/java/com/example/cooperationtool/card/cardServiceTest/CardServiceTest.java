9055ced4637e29bd7f64c2ad98bea06c3e1b18c4
JaeHoon <wogns8030@naver.com>
2023-12-29 15:18:10 +0900

    TestCode 수정 중

--- src/test/java/com/example/cooperationtool/card/cardServiceTest/CardServiceTest.java
+++ src/test/java/com/example/cooperationtool/card/cardServiceTest/CardServiceTest.java
@@ -1,52 +1,64 @@
-package com.example.cooperationtool.card.cardServiceTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
-
-import com.example.cooperationtool.domain.card.dto.CardRequestDto;
-import com.example.cooperationtool.domain.card.entity.Card;
-import com.example.cooperationtool.domain.card.repository.CardRepository;
-import com.example.cooperationtool.domain.card.service.CardService;
-import com.example.cooperationtool.global.dto.response.RootResponseDto;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-public class CardServiceTest {
-
-    @InjectMocks
-    private CardService cardService;
-
-    @Mock
-    private CardRepository cardRepository;
-
-    @Mock
-    private CardRequestDto cardRequestDto;
-
-    @BeforeEach
-    void setup() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void createTest() {
-        //given
-        given(cardRequestDto.getTitle()).willReturn("testTitle");
-
-        Card saveCard = Card.builder()
-            .id(1L)
-            .title("testTitle")
-            .build();
-
-        given(cardRepository.save(saveCard)).willReturn(saveCard);
-
-        //when
-        RootResponseDto responseDto = cardService.createCard(cardRequestDto);
-
-        //then
-        assertEquals("200",responseDto.code());
-        assertEquals("생성 완료",responseDto.message());
-    }
-}
+//package com.example.cooperationtool.card.cardServiceTest;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.BDDMockito.given;
+//
+//import com.example.cooperationtool.domain.card.dto.CardRequestDto;
+//import com.example.cooperationtool.domain.card.dto.CardResponseDto;
+//import com.example.cooperationtool.domain.card.entity.Card;
+//import com.example.cooperationtool.domain.card.repository.CardRepository;
+//import com.example.cooperationtool.domain.card.service.CardService;
+//import com.example.cooperationtool.domain.user.entity.User;
+//import com.example.cooperationtool.domain.user.repository.UserRepository;
+//import com.example.cooperationtool.global.dto.response.RootResponseDto;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//
+//public class CardServiceTest {
+//
+//    @InjectMocks
+//    private CardService cardService;
+//
+//    @Mock
+//    private CardRepository cardRepository;
+//
+//    @Mock
+//    private UserRepository userRepository;
+//
+//    @Mock
+//    private CardRequestDto cardRequestDto;
+//
+//    @Mock
+//    private User user;
+//
+//    @BeforeEach
+//    void setup() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @Test
+//    void createTest() {
+//        //given
+//        given(cardRequestDto.getTitle()).willReturn("testTitle");
+//        given(userRepository.findById(1L)).willReturn(java.util.Optional.of(user));
+//
+//        Card saveCard = Card.builder()
+//            .id(1L)
+//            .title("testTitle")
+//            .user(user)
+//            .build();
+//
+//        given(cardRepository.save(saveCard)).willReturn(saveCard);
+//
+//        //when
+//        CardResponseDto responseDto = cardService.createCard(cardRequestDto, user);
+//
+//        //then
+//        assertEquals(1L, responseDto.getCardId()); // adjust based on your actual implementation
+//        assertEquals("testTitle", responseDto.getTitle());
+//        // Add more assertions based on the actual properties of CardResponseDto
+//    }
+//}
