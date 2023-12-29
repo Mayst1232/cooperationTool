a605cbf12e6bc64d4ea48ae8e2fac0c98fc15882
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-29 20:01:52 +0900

    refactory: 컬럼이 들어가야 하는 보드를 넣어주는 기능 추가

--- src/main/java/com/example/cooperationtool/domain/column/service/ColumnService.java
+++ src/main/java/com/example/cooperationtool/domain/column/service/ColumnService.java
@@ -1,10 +1,17 @@
 package com.example.cooperationtool.domain.column.service;
 
+import static com.example.cooperationtool.global.exception.ErrorCode.NOT_FOUND_BOARD;
 import static com.example.cooperationtool.global.exception.ErrorCode.NOT_FOUND_COLUMN;
-import static com.example.cooperationtool.global.exception.ErrorCode.NOT_IN_COLUMN;
+import static com.example.cooperationtool.global.exception.ErrorCode.NOT_INVITE_USER;
 import static com.example.cooperationtool.global.exception.ErrorCode.NOT_MATCH_USER;
+
+import com.example.cooperationtool.domain.board.entity.Board;
+import com.example.cooperationtool.domain.board.entity.InviteBoard;
+import com.example.cooperationtool.domain.board.repository.BoardRepository;
+import com.example.cooperationtool.domain.board.repository.InviteBoardRepository;
 import com.example.cooperationtool.domain.column.dto.ColumnRequestDto;
 import com.example.cooperationtool.domain.column.dto.ColumnResponseDto;
+import com.example.cooperationtool.domain.column.dto.MoveColumnRequestDto;
 import com.example.cooperationtool.domain.column.entity.Columns;
 import com.example.cooperationtool.domain.column.repository.ColumnRepository;
 import com.example.cooperationtool.domain.user.entity.User;
@@ -21,22 +28,47 @@ import org.springframework.transaction.annotation.Transactional;
 public class ColumnService {
 
     private final ColumnRepository columnRepository;
+    private final InviteBoardRepository inviteBoardRepository;
+    private final BoardRepository boardRepository;
 
-    public ColumnResponseDto createColumn(ColumnRequestDto columnRequestDto, User user){
+    public ColumnResponseDto createColumn(ColumnRequestDto columnRequestDto, User user) {
+        Long boardId = columnRequestDto.getBoardId();
+        String title = columnRequestDto.getTitle();
+
+        Board board = boardRepository.findById(boardId).orElseThrow(
+            () -> new ServiceException(NOT_FOUND_BOARD)
+        );
+
+        Long priority = columnRepository.countByBoardId(boardId) + 1;
+
         Columns columns = Columns.builder()
-            .name(columnRequestDto.getName())
+            .title(title)
+            .priority(priority)
+            .board(board)
             .user(user)
             .build();
-        columnRepository.save(columns);
-        return new ColumnResponseDto(columns);
+
+        Columns savedColumns = columnRepository.save(columns);
+
+        ColumnResponseDto responseDto = ColumnResponseDto.builder()
+            .columns(savedColumns)
+            .build();
+
+        return responseDto;
     }
 
-        public void updateColumnName(Long columnId, String newName) {
-            Columns columns = columnRepository.findById(columnId)
-                .orElseThrow(() -> new ServiceException(NOT_FOUND_COLUMN));
+    public ColumnResponseDto updateColumnName(Long columnId, String title) {
+        Columns columns = columnRepository.findById(columnId)
+            .orElseThrow(() -> new ServiceException(NOT_FOUND_COLUMN));
 
-            columns.setName(newName);
-            columnRepository.save(columns);
+        columns.setTitle(title);
+        columnRepository.save(columns);
+
+        ColumnResponseDto responseDto = ColumnResponseDto.builder()
+            .columns(columns)
+            .build();
+
+        return responseDto;
     }
 
     public List<ColumnResponseDto> getAllColumns() {
@@ -52,40 +84,85 @@ public class ColumnService {
         return new ColumnResponseDto(columns);
     }
 
-    public void sortColumn(Long columnId, boolean sortUp) {
-        Columns column = columnRepository.findById(columnId)
-            .orElseThrow(() -> new ServiceException(NOT_FOUND_COLUMN));
-
-        List<Columns> columns = columnRepository.findByBoardIdOrderByOrderAsc(column.getBoard().getId());
-
-        int currentIndex = columns.indexOf(column);
-
-        if (currentIndex == -1) {
-            throw new ServiceException(NOT_IN_COLUMN);
-        }
-
-        int newIndex = sortUp ? Math.max(0, currentIndex - 1) : Math.min(columns.size() - 1, currentIndex + 1);
-
-        columns.remove(column);
-        columns.add(newIndex, column);
-
-        updateColumnOrder(columns);
-    }
-
-    private void updateColumnOrder(List<Columns> columns) {
-        for (int i = 0; i < columns.size(); i++) {
-            columns.get(i).setSort(i);
-        }
-        columnRepository.saveAll(columns);
-    }
-
     public void deleteColumn(Long columnId, User user) {
+
         Columns columns = columnRepository.findById(columnId)
             .orElseThrow(() -> new ServiceException(NOT_FOUND_COLUMN));
-        if (!user.equals(columns.getUser())) {
+
+        if (!user.getUsername().equals(columns.getUser().getUsername())) {
             throw new ServiceException(NOT_MATCH_USER);
         }
 
         columnRepository.delete(columns);
+    }
+
+//    public void sortColumn(Long columnId, boolean sortUp) {
+//        Columns column = columnRepository.findById(columnId)
+//            .orElseThrow(() -> new ServiceException(NOT_FOUND_COLUMN));
+//
+//        List<Columns> columns = columnRepository.findByBoardIdOrderByPriorityAsc(
+//            column.getBoard().getId());
+//
+//        int currentIndex = columns.indexOf(column);
+//
+//        if (currentIndex == -1) {
+//            throw new ServiceException(NOT_IN_COLUMN);
+//        }
+//
+//        int newIndex =
+//            sortUp ? Math.max(0, currentIndex - 1) : Math.min(columns.size() - 1, currentIndex + 1);
+//
+//        columns.remove(column);
+//        columns.add(newIndex, column);
+//
+//        updateColumnOrder(columns);
+//    }
+//
+//    private void updateColumnOrder(List<Columns> columns) {
+//        for (int i = 0; i < columns.size(); i++) {
+//            columns.get(i).setPriority((long) i);
+//        }
+//        columnRepository.saveAll(columns);
+//    }
+
+    @Transactional
+    public List<ColumnResponseDto> moveColumn(Long columnId, MoveColumnRequestDto requestDto,
+        User user) {
+        Columns findColumn = columnRepository.findById(columnId).orElseThrow(
+            () -> new ServiceException(NOT_FOUND_COLUMN)
+        );
+        InviteBoard checkInvite = inviteBoardRepository.findByUserAndBoard(user,
+            findColumn.getBoard()).orElseThrow(
+            () -> new ServiceException(NOT_INVITE_USER)
+        );
+
+        Long priority = findColumn.getPriority();
+        Long wantPriority = requestDto.getDestination();
+        Long moveDirection;
+        Long start;
+        Long end;
+
+        if (priority > wantPriority) {
+            moveDirection = 1L;
+            start = wantPriority;
+            end = priority;
+        } else if (priority < wantPriority) {
+            moveDirection = -1L;
+            start = priority;
+            end = wantPriority;
+        } else {
+            moveDirection = 0L;
+            start = priority;
+            end = priority;
+        }
+
+        columnRepository.moveColumn(start, end, moveDirection);
+
+        findColumn.move(wantPriority);
+
+        List<Columns> columnsList = columnRepository.findAllOrderByPriority(
+            findColumn.getBoard().getId());
+
+        return columnsList.stream().map(ColumnResponseDto::new).collect(Collectors.toList());
     }
 }
