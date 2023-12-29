da55d64252def8b3654bff1de48c0d49de464cab
JaeHoon <wogns8030@naver.com>
2023-12-29 13:17:04 +0900

    fix : test 수정 중

--- src/test/java/com/example/cooperationtool/domain/card/service/CardServiceTest.java
+++ src/test/java/com/example/cooperationtool/domain/card/service/CardServiceTest.java
@@ -1,16 +1,13 @@
 package com.example.cooperationtool.domain.card.service;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-import com.example.cooperationtool.domain.card.dto.CardRequestDto;
 import com.example.cooperationtool.domain.card.dto.CardResponseDto;
 import com.example.cooperationtool.domain.card.entity.Card;
 import com.example.cooperationtool.domain.card.repository.CardRepository;
 import com.example.cooperationtool.domain.user.entity.User;
 import com.example.cooperationtool.domain.user.entity.UserRoleEnum;
-import com.example.cooperationtool.global.dto.response.RootResponseDto;
 import com.example.cooperationtool.global.security.UserDetailsImpl;
 import java.util.Arrays;
 import java.util.List;
@@ -41,16 +38,15 @@ public class CardServiceTest {
     @DisplayName("Card 생성")
     void createTest() {
         //given
-        User mockUser = User.builder().username("testUserName").password("testPassword").role(UserRoleEnum.USER).build();
+        User mockUser = User.builder().username("testUserName").password("testPassword")
+            .role(UserRoleEnum.USER).build();
         UserDetailsImpl mockUserDetails = new UserDetailsImpl(mockUser);
         given(userDetails.getUser()).willReturn(mockUserDetails.getUser());
 
         Card saveCard = Card.builder().id(1L).title("testTitle1").build();
-        CardRequestDto requestDto = new CardRequestDto("testTitle");
 
         given(cardRepository.save(saveCard)).willReturn(saveCard);
         //when
-        RootResponseDto card = cardService.createCard(requestDto, mockUser);
 
         //then
         verify(cardRepository).save(saveCard);
@@ -58,9 +54,10 @@ public class CardServiceTest {
 
     @Test
     @DisplayName("Card 전체조회")
-    void testGets(){
+    void testGets() {
         //given
-        User mockUser = User.builder().username("testUser").password("testPassword").role(UserRoleEnum.USER).build();
+        User mockUser = User.builder().username("testUser").password("testPassword")
+            .role(UserRoleEnum.USER).build();
         UserDetailsImpl mockUserDetails = new UserDetailsImpl(mockUser);
         Card saveCard = Card.builder().id(1L).title("testTitle").build();
         Card saveCard1 = Card.builder().id(1L).title("testTitle1").build();
@@ -72,7 +69,7 @@ public class CardServiceTest {
         //when
         cardRepository.save(saveCard);
         cardRepository.save(saveCard1);
-        List<CardResponseDto> cards = cardService.getCards(mockUser);
+        List<CardResponseDto> cards = cardService.getCards();
 
         //then
         cards.forEach(card -> System.out.println(card.getTitle()));
