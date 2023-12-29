44edae79616d70923c4afda9bacee13c88f9acd7
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-29 20:01:10 +0900

    feat: add request & response Dto for Column feature

--- src/main/java/com/example/cooperationtool/domain/column/dto/ColumnResponseDto.java
+++ src/main/java/com/example/cooperationtool/domain/column/dto/ColumnResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.cooperationtool.domain.column.dto;
 
 import com.example.cooperationtool.domain.column.entity.Columns;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,19 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ColumnResponseDto {
+
+    private String title;
     private String name;
+    private Long priority;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     @Builder
-    public ColumnResponseDto(Columns columns){
-        this.name = columns.getName();
+    public ColumnResponseDto(Columns columns) {
+        this.title = columns.getTitle();
+        this.name = columns.getUser().getNickname();
+        this.priority = columns.getPriority();
+        this.createdAt = columns.getCreatedAt();
+        this.modifiedAt = columns.getModifiedAt();
     }
 }
