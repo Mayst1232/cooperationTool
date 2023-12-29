3ae816a9b956f3ddcb9dbe39b406f3a8324c6799
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-29 20:04:39 +0900

    feat: add some Repository method for Column API

--- src/main/java/com/example/cooperationtool/domain/column/repository/ColumnRepository.java
+++ src/main/java/com/example/cooperationtool/domain/column/repository/ColumnRepository.java
@@ -2,14 +2,22 @@ package com.example.cooperationtool.domain.column.repository;
 
 import com.example.cooperationtool.domain.column.entity.Columns;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ColumnRepository extends JpaRepository<Columns, Long> {
-        @Query
-        Optional<Columns> findByName(@Param("name") String name);
-        @Query
-        List<Columns> findByBoardIdOrderByOrderAsc(Long boardId);
+
+    @Query("select count(*) from Columns c "
+        + "where c.board.id = :boardId")
+    Long countByBoardId(Long boardId);
+
+    @Modifying
+    @Query("update Columns c set c.priority = c.priority + :moveDirection"
+        + " where c.priority between :wantPriority and :endPriority")
+    void moveColumn(Long wantPriority, Long endPriority, Long moveDirection);
+
+    @Query("select c from Columns as c where c.board.id = :id "
+        + "order by c.priority asc")
+    List<Columns> findAllOrderByPriority(Long id);
 }
