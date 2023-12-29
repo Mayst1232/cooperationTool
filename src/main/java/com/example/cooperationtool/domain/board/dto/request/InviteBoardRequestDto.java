9446d2d1bb2dfa2cd60b70770a20ccdf5804410f
minhyeongKang <kang7937@gmail.com>
2023-12-29 18:05:09 +0900

    feat : InviteBoardRequestDto 추가

---
+++ src/main/java/com/example/cooperationtool/domain/board/dto/request/InviteBoardRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.cooperationtool.domain.board.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class InviteBoardRequestDto {
+
+    private Long userId;
+
+}
