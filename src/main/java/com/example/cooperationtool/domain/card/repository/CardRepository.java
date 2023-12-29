b2d5eb199e22e9fd2c0115eb6abcd8bea336e1aa
JaeHoon <wogns8030@naver.com>
2023-12-29 11:35:55 +0900

    refactoring : findByIdAndUser

--- src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
+++ src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
@@ -9,5 +9,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CardRepository extends JpaRepository<Card,Long> {
 
-    Optional<Card> findByIdAndUser(Long cardId, User user);
 }
