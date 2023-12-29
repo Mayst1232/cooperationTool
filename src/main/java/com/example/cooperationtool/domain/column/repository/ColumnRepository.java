01324fd202099b0c7512c50990835de852c120bf
k <kkkjjj320320@naver.com>
2023-12-29 11:42:38 +0900

    docs: @Query 추가

--- src/main/java/com/example/cooperationtool/domain/column/repository/ColumnRepository.java
+++ src/main/java/com/example/cooperationtool/domain/column/repository/ColumnRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ColumnRepository extends JpaRepository<Columns, Long> {
-        Optional<Columns> findByName(String name);
+        @Query("SELECT c FROM Columns c WHERE c.name = :name")
+        Optional<Columns> findByName(@Param("name") String name);
         @Query
         List<Columns> findByBoardIdOrderByOrderAsc(Long boardId);
 }
