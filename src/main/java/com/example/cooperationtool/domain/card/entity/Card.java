e1bd2698d6ebdff21976aae69b22da48cd5d9a72
JaeHoon <wogns8030@naver.com>
2023-12-31 12:22:07 +0900

    feat : Columns 관계

--- src/main/java/com/example/cooperationtool/domain/card/entity/Card.java
+++ src/main/java/com/example/cooperationtool/domain/card/entity/Card.java
@@ -1,14 +1,12 @@
 package com.example.cooperationtool.domain.card.entity;
 
+import com.example.cooperationtool.domain.column.entity.Columns;
 import com.example.cooperationtool.domain.model.BaseEntity;
 import com.example.cooperationtool.domain.user.entity.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -17,6 +15,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 
 @Entity
@@ -24,15 +23,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@DynamicUpdate
 public class Card extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Columns columnsId;
 
     @Column(nullable = false)
     @Size(max = 20)
     private String title;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
-    private User user;
 
     private LocalDateTime modifiedAt;
 
