efb31d6c8a79a199e5869cdc55076261716886d8
JaeHoon <wogns8030@naver.com>
2023-12-31 12:23:19 +0900

    feat : inviteCard Entity

---
+++ src/main/java/com/example/cooperationtool/domain/card/entity/InviteCard.java
@@ -0,0 +1,26 @@
+package com.example.cooperationtool.domain.card.entity;
+
+import com.example.cooperationtool.domain.model.BaseEntity;
+import com.example.cooperationtool.domain.user.entity.User;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Builder
+public class InviteCard extends BaseEntity {
+
+    @ManyToOne
+    private Card cardId;
+
+    @ManyToOne
+    private User userId;
+
+}
