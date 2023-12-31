10fd6afaa0525ccbf65cc0c8a3637ad6f644a2c7
JaeHoon <wogns8030@naver.com>
2023-12-31 22:01:06 +0900

    git pull

---
+++ src/main/java/com/example/cooperationtool/domain/comment/controller/CommentController.java
@@ -0,0 +1,55 @@
+package com.example.cooperationtool.domain.comment.controller;
+
+import com.example.cooperationtool.domain.comment.dto.request.CommentRequestDto;
+import com.example.cooperationtool.domain.comment.dto.response.CommentResponseDto;
+import com.example.cooperationtool.domain.comment.service.CommentService;
+import com.example.cooperationtool.global.dto.response.RootResponseDto;
+import com.example.cooperationtool.global.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@RestController
+@RequestMapping("/api/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{commentId}")
+    public ResponseEntity<?> createComment(
+        @RequestBody CommentRequestDto requestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PathVariable Long cardId) {
+
+        CommentResponseDto responseDto = commentService.createComment(requestDto,userDetails.getUser(), cardId);
+        return ResponseEntity.ok(RootResponseDto.builder()
+            .code("200")
+            .message("댓글 생성 성공")
+            .data(responseDto)
+            .build()
+        );
+    }
+
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<?> updateComment(
+        @PathVariable Long commentId,
+        @RequestBody CommentRequestDto requestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CommentResponseDto responseDto = commentService.updateComment(requestDto, userDetails.getUser(), commentId);
+
+        return  ResponseEntity.ok(RootResponseDto.builder()
+            .code("200")
+            .message("댓글 수정 성공")
+            .data(responseDto)
+            .build()
+        );
+    }
+}
+
---
+++ src/main/java/com/example/cooperationtool/domain/comment/service/CommentService.java
@@ -0,0 +1,14 @@
+package com.example.cooperationtool.domain.comment.service;
+
+import com.example.cooperationtool.domain.comment.dto.request.CommentRequestDto;
+import com.example.cooperationtool.domain.comment.dto.response.CommentResponseDto;
+import com.example.cooperationtool.domain.user.entity.User;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
+public interface CommentService {
+
+    CommentResponseDto createComment(CommentRequestDto commentRequestDto, User user, Long cardId);
+
+    CommentResponseDto updateComment(CommentRequestDto commentRequestDto, User user, Long commentId);
+}
