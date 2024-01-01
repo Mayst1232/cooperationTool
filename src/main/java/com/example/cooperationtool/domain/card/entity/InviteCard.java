e8ce82933f09d2a4fdc6a3d768d99e482cb3f050
JaeHoon <wogns8030@naver.com>
2024-01-01 11:10:22 +0900

    refactoring : Entity 수정

--- src/main/java/com/example/cooperationtool/domain/card/entity/InviteCard.java
+++ src/main/java/com/example/cooperationtool/domain/card/entity/InviteCard.java
@@ -3,6 +3,10 @@ package com.example.cooperationtool.domain.card.entity;
 import com.example.cooperationtool.domain.model.BaseEntity;
 import com.example.cooperationtool.domain.user.entity.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,12 +18,18 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @Builder
-public class InviteCard extends BaseEntity {
+public class InviteCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne
+    @JoinColumn
     private Card card;
 
     @ManyToOne
+    @JoinColumn
     private User user;
 
 }
