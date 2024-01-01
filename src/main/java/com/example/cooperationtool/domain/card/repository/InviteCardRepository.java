befdbca92976420860799b97a6482b5946092bb8
JaeHoon <wogns8030@naver.com>
2024-01-01 10:10:19 +0900

    refactoring : 반환 값 수정

--- src/main/java/com/example/cooperationtool/domain/card/repository/InviteCardRepository.java
+++ src/main/java/com/example/cooperationtool/domain/card/repository/InviteCardRepository.java
@@ -1,15 +1,12 @@
 package com.example.cooperationtool.domain.card.repository;
 
-import com.example.cooperationtool.domain.card.entity.Card;
 import com.example.cooperationtool.domain.card.entity.InviteCard;
-import com.example.cooperationtool.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface InviteCardRepository extends JpaRepository<InviteCard, Long> {
 
-    void deleteByCardIdAndUserId(Card cardId, User userId);
+    void deleteByCardIdAndUserId(Long card_id, Long user_id);
 
 }
