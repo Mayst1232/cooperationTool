d1093209b79dcfff773f6f3e3dd6c2c3ba5605de
JaeHoon <wogns8030@naver.com>
2024-01-02 15:38:39 +0900

    feat : column card 관계

--- src/main/java/com/example/cooperationtool/domain/column/entity/Columns.java
+++ src/main/java/com/example/cooperationtool/domain/column/entity/Columns.java
@@ -1,14 +1,18 @@
 package com.example.cooperationtool.domain.column.entity;
 
 import com.example.cooperationtool.domain.board.entity.Board;
+import com.example.cooperationtool.domain.card.entity.Card;
 import com.example.cooperationtool.domain.model.BaseEntity;
 import com.example.cooperationtool.domain.user.entity.User;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -35,6 +39,9 @@ public class Columns extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
+
+    @OneToMany(mappedBy = "columns", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Card> card;
 
     @Builder
     private Columns(String title, Long priority, User user, Board board) {
