fa3dbfb603c65b35be7412df97ba56d79b31d198
JaeHoon <wogns8030@naver.com>
2024-01-02 15:38:14 +0900

    feat : Invite 구현

---
+++ src/main/java/com/example/cooperationtool/domain/card/entity/InviteCard.java
@@ -0,0 +1,34 @@
+package com.example.cooperationtool.domain.card.entity;
+
+import com.example.cooperationtool.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Builder
+public class InviteCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private Card card;
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+}
