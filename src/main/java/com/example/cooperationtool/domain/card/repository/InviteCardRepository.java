08ce71ffce937f5afeeb390bcbfddce517533d53
JaeHoon <wogns8030@naver.com>
2024-01-03 07:40:28 +0900

    refactor : delete findByUserId

--- src/main/java/com/example/cooperationtool/domain/card/repository/InviteCardRepository.java
+++ src/main/java/com/example/cooperationtool/domain/card/repository/InviteCardRepository.java
@@ -1,6 +1,5 @@
 package com.example.cooperationtool.domain.card.repository;
 
-import com.example.cooperationtool.domain.card.entity.Card;
 import com.example.cooperationtool.domain.card.entity.InviteCard;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +11,5 @@ public interface InviteCardRepository extends JpaRepository<InviteCard, Long> {
     void deleteByCardIdAndUserId(Long card_id, Long user_id);
 
     List<InviteCard> findByCardId(Long cardId);
-
-    Card findByUserId(Long id);
 
 }
