6ee445a908c81b64a570157cdcceb155b8cccc94
JaeHoon <wogns8030@naver.com>
2024-01-02 17:25:24 +0900

    feat : gets jpql

--- src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
+++ src/main/java/com/example/cooperationtool/domain/card/repository/CardRepository.java
@@ -2,10 +2,12 @@ package com.example.cooperationtool.domain.card.repository;
 
 import com.example.cooperationtool.domain.card.entity.Card;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -22,4 +24,16 @@ public interface CardRepository extends JpaRepository<Card,Long> {
     @Query("select c from Card as c where c.columns.id = :id "
         + "order by c.priority asc")
     List<Card> findAllOrderByPriority(Long id);
+
+    @Query("SELECT c FROM Card c "
+        + "JOIN c.inviteCard ic "
+        + "WHERE ic.user.id =: userId OR ic.user.id =:userId "
+        + "order by ic.id asc")
+    List<Card> findCardsByUserIdWithInvites(Long userId);
+
+    @Query("select c from Card c "
+        + "join c.inviteCard ic "
+        + "where c.id =: cardId "
+        + "and c.user.id =: userId and ic.user.id =: userId")
+    Optional<Card> findBycardAndUserIdWithInvites(Long cardId, Long userId);
 }
